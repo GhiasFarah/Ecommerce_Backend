94bb725d05481e27605436e99b75f836f16e6e01
Farah <farah9hias@gmail.com>
2022-08-06 18:46:31 -0700

    database connection issue resolved

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize('ecommerce_db', 'root', 'hanna7', {
+  : new Sequelize('ecommerce_db', 'root', 'Hannah@5', {
       host: 'localhost',
       port: '3306',
       dialect: 'mysql',
